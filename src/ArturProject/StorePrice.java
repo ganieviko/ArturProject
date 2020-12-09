fea9023c2ad3d92eb693c8360955838b25952580
username <ganieviko@gmail.com>
2020-12-08 23:55:50 -0500

    Create price for delivery

---
+++ src/ArturProject/StorePrice.java
@@ -0,0 +1,11 @@
+package ArturProject;
+
+public class StorePrice {
+    public static void main(String[] args) {
+        System.out.println("Write your price");
+        int a = 100;
+        int b = 124;
+        int c = a + b;
+        System.out.println("total " + c);
+    }
+}
